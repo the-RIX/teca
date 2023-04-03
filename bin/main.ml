bcc3987855a661283d55596e873c5c004ed9a568
the-rix <mc_therash@gmail.com>
2023-04-03 17:03:24 +0200

    buggy +-

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,14 @@
-let () = print_endline "Hello, World!"
+let rec get op = 
+  let input = read_line () in
+  match input with 
+  | "+" -> get (+.)
+  | "-" -> get (-.)
+  | x -> (op, float_of_string x)
+
+let rec teca op acc =
+  let _ = print_endline (string_of_float acc) in
+  let (nop, x) = get op in 
+  teca nop (nop acc x) 
+   
+
+let () = teca (+.) 0.
